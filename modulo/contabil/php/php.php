b14a2b9aa85251f897fa31e51b909dd448899f15
sergioflorencio <sergiorodrigoflorencio@gmail.com>
2015-09-21 17:54:43 -0300

    inclusão do livro razao
    inclusão da função livro razaão na classe relatórios

--- modulo/contabil/php/php.php
+++ modulo/contabil/php/php.php
@@ -1853,11 +1853,12 @@ class relatorios{
 		if($tipo==1){
 
 			echo "
-					<div>
+				<div class=' uk-width-1-1 uk-container-center uk-text-center'>
+					<div class=' uk-width-small-1-1 uk-width-medium-1-3 uk-width-large-1-4       uk-panel uk-panel-box uk-panel-box-primary uk-container-center uk-text-center' style='margin-bottom: 30px;'>
 						<h3>Filtro</h3>
-						<form class='uk-form' action='#' method='post'>
+						<form class='uk-form' action='#' method='post' style='text-align: left;'>
 							<div class='uk-grid '>
-							<div class=' uk-width-small-1-1 uk-width-medium-1-2 uk-width-large-1-3 '>
+							<div class=' uk-width-1-1'>
 								<ul class='uk-subnav' style='margin-left: -42px;'>
 									";
 										
@@ -1902,14 +1903,6 @@ class relatorios{
 									
 									
 									
-									
-							echo"
-								</ul>
-							</div>
-							
-							<div class=' uk-width-small-1-1 uk-width-medium-1-2 uk-width-large-1-3'>
-								<ul class='uk-subnav' style='margin-left: -42px;'>";
-								
 								echo "<li class='uk-grid' style='margin-top: 10px; margin-left: 30px; max-width: 280px; width: 100%;'><div class='uk-width-1-2'>";
 								$inputs->input_form_row('','cod_documento','Código de documento',"","");
 								echo"</div>
@@ -1955,121 +1948,72 @@ class relatorios{
 						</div>
 					</form>
 						<hr class='uk-article-divider'>
-				</div>";
+				</div>
+				</div>
+				
+				";
 
 		}
 		if($tipo==2){
-			echo "<div class='uk-grid uk-width-1-1' style=''>
-					<div class='uk-width-1-1' style='margin-bottom: 30px;'>
+			echo "<div class=' uk-width-1-1 uk-container-center uk-text-center'>
+					<div class=' uk-width-small-1-1 uk-width-medium-1-3 uk-width-large-1-4       uk-panel uk-panel-box uk-panel-box-primary uk-container-center uk-text-center' style='margin-bottom: 30px;'>
 						<h3>Filtro</h3>
-						<form class='uk-form' action='#' method='post'>						
-							<ul class='uk-subnav'>
-								<li>";
-									$inputs->input_form_row('00/00/0000','data_inicio','de',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
+						<form class='uk-form uk-width-1-1' action='#' method='post' style='text-align: left;'>
+							<ul class='uk-list'>
+								<li>
+									<div class='uk-grid'>
+										<div class='uk-width-1-2'>
+										";
+											$inputs->input_form_row('00/00/0000','data_inicio','de',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
+						echo "
+										</div>
+										<div class='uk-width-1-2'>
+							";
+											$inputs->input_form_row('00/00/0000','data_fim','até',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
 						echo	 	"
-								</li>
-								<li>";
-									$inputs->input_form_row('00/00/0000','data_fim','até',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
-						echo		 "
-								</li>
-								<li>";
-									$selects->aquisicao_baixa('','Relatório');
-						echo		 "
-								</li>
-								<li>";
-									$selects->cad_motivo_baixa('','Motivo de baixa');
-						echo		 "
-								</li>
-								<li>";
-									$selects->cod_grupo_patrimonio('','Grupo');
-						echo		 "
-								</li>
-								<li>";
-									$selects->filial('','Filial');
-						echo		 "
-								</li>
-								<li>";
-									$selects->tipo_patrimonio('','Tipo de patrimônio');
-						echo		 "
-								</li>
-								<li>";
-									$selects->tipo_aquisicao('','Aquisição');
-						echo		 "
-								</li>
-								<li>
-									</br>
-									<button class='uk-button uk-button-danger' type='submit' id='' ><i class='uk-icon-check'></i> Confirmar</button>
-								</li>
-							</ul>
-						</form>
-						<hr class='uk-article-divider'>
-					</div>
-				</div>";
-		
-		}
-		if($tipo==3){
-			echo "<div class='uk-grid uk-width-1-1' style=''>
-					<div class='uk-width-1-1' style='margin-bottom: 30px;'>
-						<h3>Filtro</h3>
-						<form class='uk-form' action='#' method='post'>
-							<ul class='uk-subnav'>
-								<li>";
-								$inputs->input_form_row('00/00/0000','data_inicio','de',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
-					echo	 	"
-								</li>
-								<li>
-					";
-								$inputs->input_form_row('00/00/0000','data_fim','até',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
-					echo		 "
-								</li>
-								<li>
-					";
-								$selects->cod_grupo_patrimonio('','Grupo');
-					echo		 "
-								</li>
-								<li>
-					";
-								$selects->filial('','Filial');
-					echo		 "
-								</li>
-								<li>
-									</br>
-									<button class='uk-button uk-button-danger' type='submit' id='' ><i class='uk-icon-check'></i> Confirmar</button>
-								</li>
-							</ul>
-						</form>
-						<hr class='uk-article-divider'>
-					</div>
-				</div>";
-		
-		}
-		if($tipo==4){
-			echo "<div class='uk-grid uk-width-1-1' style=''>
-					<div class='uk-width-1-1' style='margin-bottom: 30px;'>
-						<h3>Filtro</h3>
-						<form class='uk-form' action='#' method='post'>
-							<div class='uk-grid'>
-								<div class='uk-width-1-2'>";
-								$inputs->input_form_row('00/00/0000','data_inicio','de',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
-					echo	 	"</div>
-								<div class='uk-width-1-2'>";
-								$inputs->input_form_row('00/00/0000','data_fim','até',''," data-uk-datepicker={format:'DD/MM/YYYY'}");
-					echo		 "</div>
-								<div class='uk-width-1-2'>";
-								$inputs->input_form_row('','cod_item','Cod. Item',"","data-uk-datepicker={format:'DD/MM/YYYY'}");
-					echo		 "</div>
-								<div class='uk-width-1-2'>";
-								$selects->cod_grupo_patrimonio('','Grupo');
-					echo		 "</div>
-								<div class='uk-width-1-2'>";
-								$selects->filial('','Filial');
-					echo		 "</div>
-								<div class='uk-width-1-1'>
-								<hr class='uk-article-divider'>
-									<button class='uk-button uk-button-danger' type='submit' id='' ><i class='uk-icon-check'></i> Confirmar</button>
+										</div>
 								</div>
-							</div>
+								</li>
+								<li>
+									<div class='uk-grid'>
+										<div class='uk-width-1-2'>
+										";
+											$inputs->input_form_row('','conta_inicio','Conta contábil',''," ");
+						echo "
+										</div>
+										<div class='uk-width-1-2'>
+							";
+											$inputs->input_form_row('','conta_fim','até',''," ");
+						echo	 	"
+										</div>
+								</div>
+								</li>
+								<li>
+									<div class='uk-grid'>
+										<div class='uk-width-1-2'>
+										";
+											$inputs->input_form_row('','ctr_custo_inicio','Centro de custo',''," ");
+						echo "
+										</div>
+										<div class='uk-width-1-2'>
+							";
+											$inputs->input_form_row('','ctr_custo_fim','até',''," ");
+						echo	 	"
+										</div>
+								</div>
+								</li>
+								<li>
+					";
+					echo		 "
+								</li>
+
+								<li>
+									</br>
+									<button class='uk-button uk-button-danger' type='submit' id='' ><i class='uk-icon-check'></i> Confirmar</button>
+								</li>
+							</ul>
 						</form>
+						<hr class='uk-article-divider'>
 					</div>
 				</div>";
 		
@@ -2077,7 +2021,241 @@ class relatorios{
 
 	
 	}
+	function razao_conta(){
+		//	var_dump($_POST);
+			
+			function numero_conta_limpo($numero_conta){
+				
+								$numero_conta = explode (" ",$numero_conta,2);
+								$numero_conta = $numero_conta[0];
+								include "config.php";
+								$select="SELECT cod_conta,1*concat(replace(numero_conta,'.',''),REPEAT('0',tb_lr_max.lr_max-length(replace(numero_conta,'.','')))) as novo_numero_conta FROM ".$schema.".cad_conta, (SELECT max(length(replace(numero_conta,'.',''))) as lr_max FROM ".$schema.".cad_conta where cod_empresa='".$_SESSION['cod_empresa']."') as tb_lr_max where cod_empresa='".$_SESSION['cod_empresa']."' and numero_conta='".$numero_conta."'; ";
+								$resultado=mysql_query($select,$conexao) or die (mysql_error());
+								$cod_conta = mysql_fetch_array($resultado);
+								$cod_conta=$cod_conta[1];
+								return $cod_conta;
+				
+			
+			}
+			function numero_centro_custo_limpo($numero_centro_custo){
+								$numero_centro_custo = explode (" ",$numero_centro_custo,2);
+								$numero_centro_custo = $numero_centro_custo[0];
+								include "config.php";
+								$select="SELECT cod_centro_custo,1*concat(replace(numero_centro_custo,'.',''),REPEAT('0',tb_lr_max.lr_max-length(replace(numero_centro_custo,'.','')))) as novo_numero_centro_custo FROM ".$schema.".cad_centro_custo, (SELECT max(length(replace(numero_centro_custo,'.',''))) as lr_max FROM ".$schema.".cad_centro_custo where cod_empresa='".$_SESSION['cod_empresa']."') as tb_lr_max where cod_empresa='".$_SESSION['cod_empresa']."'; ";
+								$resultado=mysql_query($select,$conexao) or die (mysql_error());
+								$cod_centro_custo = mysql_fetch_array($resultado);
+								$cod_centro_custo=$cod_centro_custo[1];
+								return $cod_centro_custo;
+				
+			}
+			function tbody($data,$historico,$lancamento,$documento,$debito,$credito,$saldo,$DC){
+				return "
+						<tbody>
+							<tr>
+								<td>".$data."</td>
+								<td>".$historico."</td>
+								<td>".$lancamento."</td>
+								<td>".$documento."</td>
+								<td style='text-align: right !important;'>".$debito."</td>
+								<td style='text-align: right !important;'>".$credito."</td>
+								<td style='text-align: right !important;'>".$saldo."</td>
+								<td style='text-align: left !important;'>".$DC."</td>
+							</tr>
+						</tbody>			
+				";
+			}
+			function tbody_($nome_conta){
+				return "
+						<tbody>
+							<tr >
+								<td colspan='7'><b>".$nome_conta."</b></td>
+							</tr>
+						</tbody>			
+				";
+			}
+			
+			$tbody="";
+			$conta="";
+			
+			if(isset($_POST) and isset($_POST['data_inicio']) and isset($_POST['data_fim']) and isset($_POST['conta_inicio']) and isset($_POST['conta_fim']) and isset($_POST['ctr_custo_inicio']) and isset($_POST['ctr_custo_fim'])){
+		
+			$filtro="";
+			if($_POST['data_inicio']!="00/00/0000" or $_POST['data_fim']!="00/00/0000"){
+				$filtro.="  and ( cad_documento.data_base between '".$_POST['data_inicio']."' and '".$_POST['data_fim']."' )";
+			}
+			if($_POST['conta_inicio']!="" or $_POST['conta_fim']!=""){
+				$filtro.="  and ( novo_numero_conta between '".numero_conta_limpo($_POST['conta_inicio'])."' and '".numero_conta_limpo($_POST['conta_fim'])."' )";
+			}
+			if($_POST['ctr_custo_inicio']!="" or $_POST['ctr_custo_fim']!=""){
+				$filtro.="  and ( novo_numero_centro_custo between '".numero_centro_custo_limpo($_POST['ctr_custo_inicio'])."' and '".numero_centro_custo_limpo($_POST['ctr_custo_fim'])."' )";
+			}
+		
+		
+			include "config.php";
+			$select="
+					SELECT 
+						cad_documento.data_base as data,
+						cad_documento_item.historico,
+						cad_documento.cod_documento as lancamento,
+						cad_documento.referencia as documento,
+						cad_documento_item.montante,
+						cad_documento_item.codigo_lancamento,
 
+						concat(cad_conta.numero_conta,' - ',cad_conta.descricao) as conta,
+						concat(cad_centro_custo.numero_centro_custo,' - ',cad_centro_custo.descricao) as centro_custo,
+
+						cad_conta.numero_conta,
+						tb_novo_numero_conta.novo_numero_conta,
+
+						cad_centro_custo.numero_centro_custo,
+						tb_novo_numero_centro_custo.novo_numero_centro_custo
+
+
+					FROM 
+						contabil.cad_documento_item,
+						contabil.cad_documento,
+						contabil.cad_conta,
+						contabil.cad_centro_custo,
+						(SELECT cod_conta,1*concat(replace(numero_conta,'.',''),REPEAT('0',tb_lr_max.lr_max-length(replace(numero_conta,'.','')))) as novo_numero_conta FROM contabil.cad_conta, (SELECT max(length(replace(numero_conta,'.',''))) as lr_max FROM contabil.cad_conta where cod_empresa=1) as tb_lr_max where cod_empresa=1) as tb_novo_numero_conta,
+						(SELECT cod_centro_custo,1*concat(replace(numero_centro_custo,'.',''),REPEAT('0',tb_lr_max.lr_max-length(replace(numero_centro_custo,'.','')))) as novo_numero_centro_custo FROM contabil.cad_centro_custo, (SELECT max(length(replace(numero_centro_custo,'.',''))) as lr_max FROM contabil.cad_centro_custo where cod_empresa=1) as tb_lr_max where cod_empresa=1) as tb_novo_numero_centro_custo
+
+
+					where
+						cad_documento_item.cod_documento=cad_documento.cod_documento and
+						cad_documento_item.cod_conta=cad_conta.cod_conta and
+						cad_documento_item.cod_conta=tb_novo_numero_conta.cod_conta and
+						cad_documento_item.cod_ctr_custo=cad_centro_custo.cod_centro_custo and
+						cad_documento_item.cod_ctr_custo=tb_novo_numero_centro_custo.cod_centro_custo 
+						".$filtro."
+					order by
+						conta, data,cad_documento.cod_documento,codigo_lancamento
+			";
+			$resultado=mysql_query($select,$conexao) or die (mysql_error());
+			while($row = mysql_fetch_array($resultado))
+			{
+				if($row['codigo_lancamento']=='D'){
+					$debito=$row['montante'];
+					$credito="";
+					
+					
+				}else{
+					$debito="";
+					$credito=$row['montante'];
+
+					
+				}
+
+				$saldo="";
+				if($row['conta']!=$conta){
+					$conta=$row['conta'];					
+					$tbody.=tbody_($row['conta']);
+				}
+
+				$tbody.=tbody(data($row['data']),$row['historico'],$row['lancamento'],$row['documento'],$debito,$credito,$saldo,$row['codigo_lancamento']);
+			}	
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			//$data_inicio,$data_fim,$conta_inicio,$conta_fim,$ctr_custo_inicio,$ctr_custo_fim
+			$titulo="Livro Razão de 01/01/2000 a 31/12/2000";
+
+			
+			$thead="
+					<thead>
+						<tr>
+							<th style='width: 1.5cm;'>Data</th>
+							<th style=''>Histórico</th>
+							<th style='width: 2.0cm;'>Lançamento</th>
+							<th style='width: 2.0cm; text-align: center !important;'>Documento</th>
+							<th style='width: 2.0cm; text-align: right !important;'>Débito</th>
+							<th style='width: 2.0cm; text-align: right !important;'>Crédito</th>
+							<th style='width: 2.0cm; text-align: right !important;'>Saldo</th>
+							<th style='width: 0.5cm; text-align: left !important;'></th>
+						</tr>
+					</thead>			
+			";
+			$caption="
+					<caption style='font-size: 12px ! important; font-family: times ! important; color: rgb(0, 0, 0) ! important;'>
+						<div class='uk-grid'>
+							<div class='uk-width-1-1'>".$_SESSION['razao_social']."</div>
+							<div class='uk-width-1-1'>".$_SESSION['cnpj']."</div>
+							<div class='uk-width-1-1'>
+								<div class='uk-grid'>
+									<div class='uk-width-3-5'>".$titulo."</div>
+									<div class='uk-width-1-5'>Livro:xxx</div>
+									<div class='uk-width-1-5'>Folha:xxx</div>
+								</div>
+							</div>
+						</div>
+
+					</caption>";			
+			$tfoot="
+					<tfoot>
+						<tr>
+							<td>...</td>
+							<td>...</td>
+							<td>...</td>
+							<td>...</td>
+							<td>...</td>
+							<td>...</td>
+							<td>...</td>
+							<td>...</td>
+						</tr>
+					</tfoot>			
+			";
+
+			
+			
+			
+
+			
+		//background-color: #E36A43;
+		
+			echo "
+					<div style='width: 21.0cm;height:29.7cm; padding: 1.5cm 0.5cm;border: 1px solid #000;'>
+						<table class='uk-table uk-table-condensed' style='font-size: 12px ! important; font-family: times ! important; color: rgb(0, 0, 0) ! important;'>
+							".$caption."
+							<thead>".$thead."</thead>	
+							<tbody>".$tbody."</tbody>
+							<tfoot>".$tfoot."</tfoot>								
+						</table>
+					</div>
+					
+					";
+		
+			echo "";
+				
+				
+				
+			}
+			else{
+				
+					var_dump($_POST);
+				
+				$relatorios=new relatorios;
+				$relatorios->filtros(2);
+				
+				
+			}
+		
+
+		
+	}
 }
 class imprimir{
 	function ficha_ativo($id){
